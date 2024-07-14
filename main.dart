f7601fdd3a0838f4070e5409eb628741749e54a7
MinaYasser100 <minayasser43@gmail.com>
2024-07-15 01:55:37 +0300

    do method

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:svg';
+
 void main() {
   int number = 0;
   String name = 'Ahmed';
@@ -13,4 +15,8 @@ void main() {
   print(map);
   List<int> myList = [99, 55, 6, 77];
   myList.add(55);
+}
+
+void getMyname() {
+  print('Mina');
 }
